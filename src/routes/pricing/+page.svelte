a5c5dcc30a20313682b3b98d04a7bd476769d75d
jamienahx <jamie.nahx@gmail.com>
2025-07-17 20:15:01 +0800

    sign in page

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -24,6 +24,7 @@
 
 
   <div class="flex flex-wrap justify-center mt-3">
+ 
     {#each choices as element}
       <div
         class={`px-4 py-2 font-semibold bg-red-400 border border-black text-white
@@ -35,17 +36,21 @@
         {element}
         </div>
     {/each}
-  </div>
-
-  <!-- Treatment Cards -->
-    <div class="flex flex-wrap gap-x-5 gap-y-5 m-5">
-      {#each priceMenu as element}
-        <div class="bg-red-800 border-white text-white text-md font-semi-bold rounded-2xl">
-        <div class="m-2"><strong>{element.english_name}</strong></div>
-        <div class="m-2">{element.description}</div>
-        <div class="text-right m-2"><strong>${element.starting_price}</strong></div>
-        </div>
-      {/each}
     </div>
+ 
+
+ <!-- create an "outer container and make it flex. inner container will be centered with 16 pexels of space on left and right side of inner container -->
+<div class="w-full flex justify-center px-4">  
+  <!-- Limit the width on larger screens and stack cards vertically -->
+  <div class="w-full max-w-4xl flex flex-col gap-4 mt-5">
+    {#each priceMenu as element}
+      <div class="bg-red-800 text-white text-md font-semibold rounded-xl p-4 shadow">
+        <div class="mb-1 text-lg font-bold">{element.english_name}</div>
+        <div class="mb-2 text-sm">{element.description}</div>
+        <div class="text-right text-md font-bold">${element.starting_price}</div>
+      </div>
+    {/each}
+  </div>
+</div>
 
     
