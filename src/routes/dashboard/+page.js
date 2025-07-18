02956124434efabac1f2254a12fc1975c8547127
jamienahx <jamie.nahx@gmail.com>
2025-07-18 14:59:49 +0800

    dashboard page created

---
+++ src/routes/dashboard/+page.js
@@ -0,0 +1,14 @@
+export async function load () {
+
+    const token = localStorage.getItem('token');
+
+if(!token) {
+
+    //if no token is found then redirect to login
+    window.location.href = '/login';
+    return;
+}
+
+return{};
+
+}
