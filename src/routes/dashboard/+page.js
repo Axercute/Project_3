370f1e091879f58f0590dfecbe3c2c89c902e62d
jamienahx <jamie.nahx@gmail.com>
2025-07-19 17:27:12 +0800

    synced FE with BE protection of dashboard route

--- src/routes/dashboard/+page.js
+++ src/routes/dashboard/+page.js
@@ -1,14 +1,14 @@
-export async function load () {
+//export async function load () {
 
-    const token = localStorage.getItem('token');
+    //const token = localStorage.getItem('token');
 
-if(!token) {
+//if(!token) {
 
     //if no token is found then redirect to login
-    window.location.href = '/login';
-    return;
-}
+    //window.location.href = '/login';
+    //return;
+//}
 
-return{};
+//return{};
 
-}
+//}
